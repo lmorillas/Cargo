abe55e53242e11beba63d8b359cf29608e269449
Luis Miguel Morillas <morillas@gmail.com>
2015-07-22 00:53:25 +0200

    collapsible

--- formats/CargoExhibitFormat.php
+++ formats/CargoExhibitFormat.php
@@ -87,8 +87,9 @@ END;
             // Add on "px", if no unit is defined.
             foreach ($facets as $f) {
                  $attrs = array(
-                    'class' => " mw-collapsible mw-collapsed",
                     'data-ex-role' => "facet",
+                    'data-ex-collapsible' => "true",
+                    'data-ex-collapsed' => "true,"
                     'data-ex-expression' => '.' . $f,
                     'data-ex-show-missing' => 'false',
                     'data-ex-facet-label' => ucfirst($f)
