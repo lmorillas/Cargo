d80ada2746c1252d011ed1aa02daef32a7ea8981
Luis Miguel Morillas <morillas@gmail.com>
2015-09-08 17:31:12 +0200

    precise autoCreate

--- libs/ext.cargo.exhibit.js
+++ libs/ext.cargo.exhibit.js
@@ -5,14 +5,18 @@
 
 // Exhibit_urlPrefix = "http://beta.programaseducativosaragon.es/mw126/extensions/Cargo/libs/Exhibit/";
 Exhibit_urlPrefix = "http://api.simile-widgets.org/exhibit/HEAD/";
+Exhibit_TimeExtension_urlPrefix = Exhibit_urlPrefix;
+Exhibit_MapExtension_urlPrefix = Exhibit_urlPrefix;
+
 //ex_url = "http://beta.programaseducativosaragon.es/mw126/extensions/Cargo/libs/Exhibit/exhibit-api.js"
 
-ex_url = "http://api.simile-widgets.org/exhibit/HEAD/exhibit-api.js?autoCreate=false";
+ex_url = "http://api.simile-widgets.org/exhibit/HEAD/exhibit-api.js";
 
 jQuery.getScript(ex_url);
 
 jQuery(document).on("scriptsLoaded.exhibit", function(evt) {
     Exhibit.params.autoCreate = false;
+    console.log(Exhibit.params);
 });
 
 jQuery(document).on("staticComponentsRegistered.exhibit", function(evt) {
