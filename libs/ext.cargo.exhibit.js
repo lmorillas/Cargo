1359a5f8d1d43263ce8f8ac184483bb2378d21c3
Luis Miguel Morillas <morillas@gmail.com>
2015-09-08 22:50:12 +0200

    typo in base urls

--- libs/ext.cargo.exhibit.js
+++ libs/ext.cargo.exhibit.js
@@ -5,18 +5,20 @@
 
 // Exhibit_urlPrefix = "http://beta.programaseducativosaragon.es/mw126/extensions/Cargo/libs/Exhibit/";
 Exhibit_urlPrefix = "http://api.simile-widgets.org/exhibit/HEAD/";
-Exhibit_TimeExtension_urlPrefix = Exhibit_urlPrefix;
-Exhibit_MapExtension_urlPrefix = Exhibit_urlPrefix;
+Exhibit_TimeExtension_urlPrefix = Exhibit_urlPrefix + "extensions/time/";
+Exhibit_MapExtension_urlPrefix = Exhibit_urlPrefix + "extensions/map/";
 
+// TODO posibility of local files
 //ex_url = "http://beta.programaseducativosaragon.es/mw126/extensions/Cargo/libs/Exhibit/exhibit-api.js"
 
 ex_url = "http://api.simile-widgets.org/exhibit/HEAD/exhibit-api.js";
 
+// get exhibit-api js
 jQuery.getScript(ex_url);
 
+// important: autoCreate=false
 jQuery(document).on("scriptsLoaded.exhibit", function(evt) {
     Exhibit.params.autoCreate = false;
-    console.log(Exhibit.params);
 });
 
 jQuery(document).on("staticComponentsRegistered.exhibit", function(evt) {
