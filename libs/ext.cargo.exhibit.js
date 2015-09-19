7f4322b5d14d2a508db6d43880ba6f26bdc96bec
Luis Miguel Morillas <morillas@gmail.com>
2015-09-19 23:01:50 +0200

    testing cache

--- libs/ext.cargo.exhibit.js
+++ libs/ext.cargo.exhibit.js
@@ -7,24 +7,26 @@
 Exhibit_urlPrefix = "http://api.simile-widgets.org/exhibit/HEAD/";
 Exhibit_TimeExtension_urlPrefix = Exhibit_urlPrefix + "extensions/time/";
 Exhibit_MapExtension_urlPrefix = Exhibit_urlPrefix + "extensions/map/";
+Timeline_ajax_url="http://YOUR_SERVER/javascripts/timeline/timeline_ajax/simile-ajax-api.js";
+Timeline_urlPrefix='http://YOUR_SERVER/javascripts/timeline/timeline_js/';
+Timeline_parameters='bundle=true';
+
 
 window.Exhibit_parameters="?autoCreate=false";
 
 // TODO posibility of local files
 //ex_url = "http://beta.programaseducativosaragon.es/mw126/extensions/Cargo/libs/Exhibit/exhibit-api.js"
 
-ex_url = "http://api.simile-widgets.org/exhibit/HEAD/exhibit-api.js?autoCreate=false";
+ex_url = "http://api.simile-widgets.org/exhibit/HEAD/exhibit-api.js";
 jQuery("#loading_exhibit").show();
-jQuery.getScript(ex_url);
-/*
+//jQuery.getScript(ex_url);
+
 jQuery.ajax({
     url: ex_url,
     dataType: "script",
     cache: true
 });
-*/
 
-// important: autoCreate=false
 jQuery(document).on("scriptsLoaded.exhibit", function(evt) {
     jQuery("#loading_exhibit").hide();
 });
