b16a3b7fefc65ddfd349dd0ce704cbc83f3f685c
Luis Miguel Morillas <morillas@gmail.com>
2015-09-19 16:36:34 +0200

    js cache

--- libs/ext.cargo.exhibit.js
+++ libs/ext.cargo.exhibit.js
@@ -11,16 +11,16 @@ Exhibit_MapExtension_urlPrefix = Exhibit_urlPrefix + "extensions/map/";
 // TODO posibility of local files
 //ex_url = "http://beta.programaseducativosaragon.es/mw126/extensions/Cargo/libs/Exhibit/exhibit-api.js"
 
-ex_url = "http://api.simile-widgets.org/exhibit/HEAD/exhibit-api.js";
-
+ex_url = "http://api.simile-widgets.org/exhibit/HEAD/exhibit-api.js?autoCreate=false";
+jQuery("#loading_exhibit").show();
+jQuery.getScript(ex_url);
+/*
 jQuery.ajax({
-    beforeSend: function(){
-        jQuery("#loading_exhibit").show();
-    },
     url: ex_url,
     dataType: "script",
     cache: true
 });
+*/
 
 // important: autoCreate=false
 jQuery(document).on("scriptsLoaded.exhibit", function(evt) {
